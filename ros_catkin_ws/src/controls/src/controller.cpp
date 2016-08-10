28c48205281b05027c237a1362e00b0bb16fd2f8
Daniel Delago <ddelago0596@gmail.com>
2016-08-10 17:01:19 +0000

    Fixed Serial Command Overflow

--- ros_catkin_ws/src/controls/src/controller.cpp
+++ ros_catkin_ws/src/controls/src/controller.cpp
@@ -5,7 +5,7 @@
  * 
  * Node: controller
  * Subscriptions: controller_raw
- * Publishing Topics: turtle1/cmd_vel, cmd_vel
+ * Publishing Topics: cmd_vel
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,22 +15,23 @@
 #include <geometry_msgs/Twist.h>
 
 class Controls
-{
+{   
     public: Controls();
     private:void CallBack(const sensor_msgs::Joy::ConstPtr& joy);		
 	    void Drive(const sensor_msgs::Joy::ConstPtr& joy);				//Add more functions as needed 
     ros::NodeHandle n;
     ros::Subscriber controller_sub;
-    ros::Publisher Joy2Turtlesim_pub, Drive_pub; 					//Add more publishers as needed
+    ros::Publisher Joy2Turtlesim_pub, Drive_pub; 					    //Add more publishers as needed
+    
+    
 };
 
 Controls::Controls()
 {      
-    controller_sub 	= n.subscribe("controller_raw",100, &Controls::CallBack, this); 
-    //Joy2Turtlesim_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel",100);		//Drive Testing
-    Drive_pub 	= n.advertise<geometry_msgs::Twist>("cmd_vel",100);				//New Drive Testing
-    //Val_pub  	= n.advertise<std_msgs::Float32MultiArray>("cmd_vel",100);		
-    //pubs..
+    controller_sub 	= n.subscribe("controller_raw",1, &Controls::CallBack, this); 	//Queue size of 1
+    //Joy2Turtlesim_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel",100);	//Drive Testing
+    Drive_pub 	= n.advertise<geometry_msgs::Twist>("cmd_vel",1);			        //Queue size of 1
+    //Val_pub  	= n.advertise<std_msgs::Float32MultiArray>("cmd_vel",100);		    //Publish Values into Array
 }
 
 //CallBack Function
@@ -40,42 +41,41 @@ void Controls::CallBack(const sensor_msgs::Joy::ConstPtr& joy)				//Add all func
 }
 
 //Controller Functions
-void Controls::Drive(const sensor_msgs::Joy::ConstPtr& joy)				
+void Controls::Drive(const sensor_msgs::Joy::ConstPtr& joy)				    //Inputs are -1.0 to 1.0	
 {
-    float x_axis = -1.0 * (joy->axes[0]);
-    int x = joy->buttons[1];
-    float forward = (joy->axes[4]);
-    float reverse = (joy->axes[3]);
+    float x_axis = -1.0 * (joy->axes[0]);	//Left Stick
+    int x = joy->buttons[1];			    //X Button
+    float forward = (joy->axes[4]);		    //Right Trigger R2
+    float reverse = (joy->axes[3]);		    //Left Trigger L2
     float Drive; 
-    forward = -1.0*((forward-1.0)/2.0);  // 2.0;
-    reverse = (reverse-1.0)/2.0; // 2.0;
-    Drive = forward + reverse;
+    forward = -1.0*((forward-1.0)/2.0);		//Converts values  
+    reverse = (reverse-1.0)/2.0; 	    	//Converts values
+    Drive = forward + reverse;		    	//Combines Forward and Revers Values for net Drive Speed and Direction
     
-    geometry_msgs::Twist msg;								
+    geometry_msgs::Twist msg;			    //Publish Values as Twist Message: Linear and Angular with xyz Values				
     msg.linear.x=(Drive);
     msg.angular.z=(x_axis);
     Drive_pub.publish(msg);
     
-    //Publish Needed Controller Values to Apropriate Topics
-
-    /*std_msgs::Float32MultiArray msg;  
+    /*std_msgs::Float32MultiArray msg;  	//Publish Values to an Array
     msg.data.push_back(Drive);
     msg.data.push_back(x_axis);
     msg.data.push_back(x);
     Val_pub.publish(msg);
     */
-
-    
 }
 
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "controller");  						//Create node named "controller"
+    ros::init(argc, argv, "controller");  	//Create node named "controller"
     Controls controls;   
 
-    ros::spin();
+    ros::Rate loop_rate(2.5);			    //Publishes x times per second
+    while(ros::ok()){
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 }
-
 
 
